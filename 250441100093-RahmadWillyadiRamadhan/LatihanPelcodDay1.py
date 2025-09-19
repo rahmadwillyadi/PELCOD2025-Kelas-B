d7483833a2d6442f1f98d1f915f9447a67957a78
rahmadwillyadi <rahmadwillyadi123@gmail.com>
2025-09-19 20:31:00 +0700

    Add files via upload

---
+++ 250441100093-RahmadWillyadiRamadhan/LatihanPelcodDay1.py
@@ -0,0 +1,29 @@
+# nama = "Rahmad willyadi Ramadhan"
+# nim = 250441100093
+# semester = 1
+
+# print("====BBIODATA MAHASISWA====")
+# print("Hallo, nama saya : "+ nama + "\n")
+# print(f"NIM saya : {nim}\n")
+# print("saya berkuliah di semester : {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b 
+# e = a * b 
+# f = a / b
+# g = a % b
+# print("Hasil Penjumlahan:", c)
+# print("Hasil Pengurangan:", d)
+# print("Hasil Perkalian:", e)
+# print("Hasil Pembagian:", f)
+# print("Hasil sisa bagi:", g)
+
+nama = input("masukkan nama anda: ")
+umur = input("masukkan umur anda: ")
+buah = input("masukkan buah favorit anda: ")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur + " tahun")
+print("buah favorit saya adalah " + buah)
